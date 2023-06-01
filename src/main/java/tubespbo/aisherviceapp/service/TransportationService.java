bc9d4cdd8cc09b01bc02c7f504e2eb5db0e6f9f4
Arya Ashari <aryaashari100@gmail.com>
2023-06-02 02:56:14 +0700

    create TransportationService, add function getAll

---
+++ src/main/java/tubespbo/aisherviceapp/service/TransportationService.java
@@ -0,0 +1,25 @@
+package tubespbo.aisherviceapp.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import tubespbo.aisherviceapp.entity.Transportation;
+import tubespbo.aisherviceapp.repository.CustomerRepository;
+import tubespbo.aisherviceapp.repository.TransportationRepository;
+
+@Service
+public class TransportationService {
+    
+    @Autowired
+    private TransportationRepository transportationRepo;
+
+    @Autowired
+    private CustomerRepository customerRepository;
+
+    public List<Transportation> getAll() {
+        return this.transportationRepo.findAll();
+    }
+
+}
