59b31ca9929d2cf29dfa220ee85c304a8e3473e1
Aisha Nuryan <91200696+aishaptrn@users.noreply.github.com>
2023-06-12 12:45:50 +0700

    montir update

--- src/main/java/tubespbo/aisherviceapp/entity/Montir.java
+++ src/main/java/tubespbo/aisherviceapp/entity/Montir.java
@@ -2,6 +2,8 @@ package tubespbo.aisherviceapp.entity;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
@@ -16,20 +18,25 @@ import lombok.Setter;
 @Entity
 @Table(name = "montir")
 public class Montir {
-    
+   
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id_montir")
     private Long idMontir;
 
+    @Column(name = "name")
     private String name;
 
     @Column(name = "no_hp")
     private String noHp;
 
+    @Column(name = "alamat")
     private String alamat;
 
+    @Column(name = "email")
     private String email;
 
+    @Column(name = "shift")
     private String shift;
 
 }
