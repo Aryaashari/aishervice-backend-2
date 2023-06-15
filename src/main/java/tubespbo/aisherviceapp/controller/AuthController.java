ecfe8df6f11bb6cfd6cd633a48eade5784e88a8c
Arya Ashari <aryaashari100@gmail.com>
2023-06-15 16:33:47 +0700

    add feature login (belum beres)

--- src/main/java/tubespbo/aisherviceapp/controller/AuthController.java
+++ src/main/java/tubespbo/aisherviceapp/controller/AuthController.java
@@ -1,14 +1,39 @@
 package tubespbo.aisherviceapp.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import jakarta.servlet.http.HttpServletRequest;
+import tubespbo.aisherviceapp.entity.Admin;
+import tubespbo.aisherviceapp.service.AdminService;
+import tubespbo.aisherviceapp.service.AuthService;
 
 @Controller
 public class AuthController {
+
+    @Autowired
+    private AdminService adminService;
+
+    @Autowired
+    private AuthService authService;
     
     @GetMapping("/login")
     public String loginView() {
         return "auth/login";
+    }
+
+    @PostMapping("/login")
+    public String login(@RequestParam("username") String username, @RequestParam("password") String password, HttpServletRequest request) {
+        boolean isTrue = this.authService.login(username, password, request);
+
+        if (isTrue) {
+            return "redirect:/";
+        }
+        
+        return "redirect:/login";
     }
 
 }
