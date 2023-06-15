15d95b6dedfea6456bcffff21377edbeb5253743
rezadhied <rezadhied@student.telkomuniversity.ac.id>
2023-06-15 10:19:50 +0700

    progress update

--- src/main/java/tubespbo/aisherviceapp/service/ProgressService.java
+++ src/main/java/tubespbo/aisherviceapp/service/ProgressService.java
@@ -1,12 +1,15 @@
 package tubespbo.aisherviceapp.service;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cglib.core.Local;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import tubespbo.aisherviceapp.entity.Progress;
+import tubespbo.aisherviceapp.entity.Transaksi;
 import tubespbo.aisherviceapp.model.ProgressCreateRequest;
 import tubespbo.aisherviceapp.repository.ProgressRepository;
 
@@ -28,6 +31,14 @@ public class ProgressService{
         objProgress.setWaktu_progress(request.getWaktu_progress());
 
         this.progressRepo.save(objProgress);
+    }
+
+    @Transactional
+    public void updateCatatan(Long id) {
+        Progress progress = this.progressRepo.findById(id).orElse(null);
+        progress.setCatatan_proses("Selesai");
+        progress.setWaktu_progress(LocalDateTime.now());
+        this.progressRepo.save(progress);
     }
 
     @Transactional
